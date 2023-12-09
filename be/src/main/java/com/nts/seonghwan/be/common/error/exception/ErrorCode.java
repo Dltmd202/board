cb3f15701293200d1d9f17d9a903b7e109cb31de
Dltmd202 <dltmd202@gmail.com>
2023-12-09 18:21:38 +0900

    [BE] feat: create exception related to user registration
    - create InvalidRepeatedPasswordException
    - create DuplicatedEmailException

--- be/src/main/java/com/nts/seonghwan/be/common/error/exception/ErrorCode.java
+++ be/src/main/java/com/nts/seonghwan/be/common/error/exception/ErrorCode.java
@@ -5,6 +5,10 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
+    // 인증 관련 에러
+    INVALID_REPEATED_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
+    DUPLICATED_EMAIL(HttpStatus.BAD_REQUEST, "이미 가입된 이메일 입니다."),
+
     // 서버 내부 에러
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에 오류가 발생했습니다.");
 
