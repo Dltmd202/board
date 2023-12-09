77eb78d5aba5f75bf1a43d250d67fb0c29e68184
Dltmd202 <dltmd202@gmail.com>
2023-12-09 20:05:29 +0900

    [BE] feat: update UserTest
    - User login method verification test

--- be/src/test/java/com/nts/seonghwan/be/user/entities/UserTest.java
+++ be/src/test/java/com/nts/seonghwan/be/user/entities/UserTest.java
@@ -6,6 +6,8 @@ import com.nts.seonghwan.be.user.service.PasswordEncoder;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 
 
 class UserTest {
@@ -25,5 +27,38 @@ class UserTest {
         // then
         assertThat(user.getPassword()).isNotEqualTo("1234");
         assertThat(passwordEncoder.matches("1234", user.getPassword())).isEqualTo(true);
+    }
+
+    @Test
+    void PasswordEncoder로_암호화된_비밀번호를_검증할_수_있다() {
+        // given
+        User user = User.builder()
+                .email("abc123@abc.com")
+                .password("1234")
+                .build();
+
+        user.encryptPassword(passwordEncoder);
+
+        // when
+        // then
+        assertThatCode(() ->user.login(passwordEncoder, "1234", new InvalidAuthenticationException()))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    void PasswordEncoder로_잘못된_비밀번호가_입력되면_인자로_준_예외를_발생시킨다() {
+        // given
+        User user = User.builder()
+                .email("abc123@abc.com")
+                .password("1234")
+                .build();
+
+        user.encryptPassword(passwordEncoder);
+
+        // when
+        // then
+        assertThatThrownBy(() ->user.login(passwordEncoder, "1235", new InvalidAuthenticationException()))
+                .isInstanceOf(InvalidAuthenticationException.class);
+
     }
 }
