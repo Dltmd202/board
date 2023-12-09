e2c46d62f1dbe9140cc5803a1fbd5af9d00fe50b
Dltmd202 <dltmd202@gmail.com>
2023-12-09 20:45:08 +0900

    [BE] feat: update UserApiController
    - login method to authenticate the user

--- be/src/main/java/com/nts/seonghwan/be/user/controller/UserApiController.java
+++ be/src/main/java/com/nts/seonghwan/be/user/controller/UserApiController.java
@@ -2,6 +2,8 @@ package com.nts.seonghwan.be.user.controller;
 
 import com.nts.seonghwan.be.common.utils.ApiUtils;
 import com.nts.seonghwan.be.common.utils.ApiUtils.ApiResult;
+import com.nts.seonghwan.be.user.dto.SigninRequest;
+import com.nts.seonghwan.be.user.dto.SigninResponse;
 import com.nts.seonghwan.be.user.dto.SignupRequest;
 import com.nts.seonghwan.be.user.dto.SignupResponse;
 import com.nts.seonghwan.be.user.service.UserService;
@@ -10,6 +12,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static com.nts.seonghwan.be.common.constants.SessionConstants.LOGIN_USER;
 
 @RestController
 @RequiredArgsConstructor
@@ -34,4 +40,18 @@ public class UserApiController {
                 .status(HttpStatus.OK)
                 .body(ApiUtils.success(userService.validateEmail(email)));
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<ApiResult<SigninResponse>> login(
+            HttpServletRequest request,
+            @Validated @RequestBody SigninRequest signinRequest
+    ){
+        SigninResponse login = userService.login(signinRequest);
+        request.getSession().setAttribute(LOGIN_USER, login.getId());
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ApiUtils.success(login));
+    }
+
 }
