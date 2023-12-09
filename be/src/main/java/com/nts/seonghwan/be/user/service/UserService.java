85157f9c4665dba648d69b117483eff89a3a3a19
Dltmd202 <dltmd202@gmail.com>
2023-12-09 20:06:03 +0900

    [BE] feat: update UserService
    - login method to authenticate the user

--- be/src/main/java/com/nts/seonghwan/be/user/service/UserService.java
+++ be/src/main/java/com/nts/seonghwan/be/user/service/UserService.java
@@ -1,9 +1,13 @@
 package com.nts.seonghwan.be.user.service;
 
+import com.nts.seonghwan.be.common.error.exception.BusinessException;
+import com.nts.seonghwan.be.user.dto.SigninRequest;
+import com.nts.seonghwan.be.user.dto.SigninResponse;
 import com.nts.seonghwan.be.user.dto.SignupRequest;
 import com.nts.seonghwan.be.user.dto.SignupResponse;
 import com.nts.seonghwan.be.user.entities.User;
 import com.nts.seonghwan.be.user.exception.DuplicatedEmailException;
+import com.nts.seonghwan.be.user.exception.InvalidAuthenticationException;
 import com.nts.seonghwan.be.user.exception.InvalidRepeatedPasswordException;
 import com.nts.seonghwan.be.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -32,6 +36,14 @@ public class UserService {
         return !userRepository.existsByEmail(email);
     }
 
+    @Transactional(readOnly = true)
+    public SigninResponse login(SigninRequest signinRequest) {
+        User user = getUserByEmail(signinRequest.getEmail(), new InvalidAuthenticationException());
+        user.login(passwordEncoder, signinRequest.getPassword(), new InvalidAuthenticationException());
+
+        return SigninResponse.from(user);
+    }
+
     private void validateSignupRequestForm(SignupRequest signupRequest) {
         if(!signupRequest.isPasswordEqualToRepeatedPassword())
             throw new InvalidRepeatedPasswordException();
@@ -40,5 +52,10 @@ public class UserService {
     private void validateDuplicatedEmail(String email) {
         if(userRepository.existsByEmail(email))
             throw new DuplicatedEmailException();
+    }
+
+    private User getUserByEmail(String email, BusinessException exception) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> exception);
     }
 }
