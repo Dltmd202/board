8fb2b0e2a2badbead88ac5daeb71ad2f27c16e99
Dltmd202 <dltmd202@gmail.com>
2023-12-09 20:03:21 +0900

    [BE] feat: create InvalidAuthenticationException

--- be/src/main/java/com/nts/seonghwan/be/common/error/exception/ErrorCode.java
+++ be/src/main/java/com/nts/seonghwan/be/common/error/exception/ErrorCode.java
@@ -8,6 +8,7 @@ public enum ErrorCode {
     // 인증 관련 에러
     INVALID_REPEATED_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
     DUPLICATED_EMAIL(HttpStatus.BAD_REQUEST, "이미 가입된 이메일 입니다."),
+    INVALID_AUTHENTICATION(HttpStatus.UNAUTHORIZED, "로그인에 실패했습니다."),
 
     // 서버 내부 에러
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에 오류가 발생했습니다.");
