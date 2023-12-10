aabace0b7ab54d50464fe1c216445336afc7fbf9
Dltmd202 <dltmd202@gmail.com>
2023-12-10 10:37:10 +0900

    [FE] feat: Update api/index.js
    - setting base authenticated request

--- fe/src/api/index.js
+++ fe/src/api/index.js
@@ -1,12 +1,29 @@
 import axios from "axios";
 
-
 const createRequest = () => {
-    const instance = axios.create({
-        baseURL: 'http://localhost:8080',
-        timeout: 3000
-    });
-    return instance;
+  const instance = axios.create({
+    baseURL: 'http://localhost:8080',
+    timeout: 3000
+  });
+  return instance;
 }
 
+const setAuthInterceptors = () => {
+  const instance = createRequest();
+  const sessionId = localStorage.getItem("TOKEN");
+
+  instance.interceptors.request.use(
+    (config) => {
+      config.headers["Authorization"] = `Bearer ${sessionId}`;
+      return config;
+    },
+    (error) => Promise.reject(error.response),
+  );
+
+  return instance;
+};
+
+
 export const request = createRequest();
+
+export const authRequest = setAuthInterceptors();
