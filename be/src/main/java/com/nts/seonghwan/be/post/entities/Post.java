1cb8557b15b78572f3220124cc25fe0a9bfc78bb
Dltmd202 <dltmd202@gmail.com>
2023-12-10 11:27:16 +0900

    [BE] feat: Create Post.java
    - jpa entity for post

---
+++ be/src/main/java/com/nts/seonghwan/be/post/entities/Post.java
@@ -0,0 +1,31 @@
+package com.nts.seonghwan.be.post.entities;
+
+import com.nts.seonghwan.be.user.entities.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = PROTECTED)
+public class Post {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
