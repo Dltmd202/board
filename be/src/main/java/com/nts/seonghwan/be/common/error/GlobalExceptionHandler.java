c3877b62daa56ed972539972c4f0be2a79dacd77
Dltmd202 <dltmd202@gmail.com>
2023-12-12 01:26:52 +0900

    [BE] fix: edit GlobalExceptionHandler
    - MethodArgumentNotValidException exception filter

--- be/src/main/java/com/nts/seonghwan/be/common/error/GlobalExceptionHandler.java
+++ be/src/main/java/com/nts/seonghwan/be/common/error/GlobalExceptionHandler.java
@@ -5,8 +5,12 @@ import com.nts.seonghwan.be.common.utils.ApiUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.nts.seonghwan.be.common.utils.ApiUtils.error;
 
@@ -23,13 +27,27 @@ public class GlobalExceptionHandler {
         return newResponse(e, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    private ResponseEntity<ApiUtils.ApiResult<?>> newResponse(Throwable throwable, HttpStatus status) {
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return newResponse(handleValidationException(e), HttpStatus.BAD_REQUEST);
+    }
+
+    private ResponseEntity<ApiUtils.ApiResult<String>> newResponse(Throwable throwable, HttpStatus status) {
         return newResponse(throwable.getMessage(), status);
     }
 
-    private ResponseEntity<ApiUtils.ApiResult<?>> newResponse(String message, HttpStatus status) {
+    private <T> ResponseEntity<ApiUtils.ApiResult<T>> newResponse(T message, HttpStatus status) {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", "application/json");
         return new ResponseEntity<>(error(message, status), headers, status);
     }
+
+    private Map<String, String> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(fieldError -> {
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        });
+        return errors;
+    }
+
 }
