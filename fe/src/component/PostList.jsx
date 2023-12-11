b9e755032746c2b854e22c7f8314e5c50bca3c71
Dltmd202 <dltmd202@gmail.com>
2023-12-12 04:14:18 +0900

    [FE] feat: Separating the item component of the post list

--- fe/src/component/PostList.jsx
+++ fe/src/component/PostList.jsx
@@ -1,5 +1,14 @@
+import {useNavigate} from "react-router-dom";
+
 const PostList = () => {
+  const navigate = useNavigate();
   const titleStyle = {minWidth: '200px'};
+  const defaultPost = {
+    title: '제목1',
+    user: 'abc123@abc.com',
+    createdAt: '2023.12.12',
+    postId: 'ea642987-0e39-4cfc-9d4f-f8356b0e8658',
+  }
 
   return (
     <div>
@@ -11,91 +20,22 @@ const PostList = () => {
           <th scope="col">작성자</th>
           <th scope="col">작성일시</th>
           <th scope="col">댓글</th>
-          <th scope="col">조회수</th>
           <th scope="col">좋아요</th>
+          <th scope="col">조회수</th>
         </tr>
         </thead>
         <tbody>
-        <tr>
-          <td>제목1</td>
-          <td>작성자1</td>
-          <td>2023.12.12</td>
-          <td>10</td>
-          <td>1000</td>
-          <td>3</td>
-        </tr>
-        <tr>
-          <td>제목1</td>
-          <td>작성자1</td>
-          <td>2023.12.12</td>
-          <td>10</td>
-          <td>1000</td>
-          <td>3</td>
-        </tr>
-        <tr>
-          <td>제목1</td>
-          <td>작성자1</td>
-          <td>2023.12.12</td>
-          <td>10</td>
-          <td>1000</td>
-          <td>3</td>
-        </tr>
-        <tr>
-          <td>제목1</td>
-          <td>작성자1</td>
-          <td>2023.12.12</td>
-          <td>10</td>
-          <td>1000</td>
-          <td>3</td>
-        </tr>
-        <tr>
-          <td>제목1</td>
-          <td>작성자1</td>
-          <td>2023.12.12</td>
-          <td>10</td>
-          <td>1000</td>
-          <td>3</td>
-        </tr>
-        <tr>
-          <td>제목1</td>
-          <td>작성자1</td>
-          <td>2023.12.12</td>
-          <td>10</td>
-          <td>1000</td>
-          <td>3</td>
-        </tr>
-        <tr>
-          <td>제목1</td>
-          <td>작성자1</td>
-          <td>2023.12.12</td>
-          <td>10</td>
-          <td>1000</td>
-          <td>3</td>
-        </tr>
-        <tr>
-          <td>제목1</td>
-          <td>작성자1</td>
-          <td>2023.12.12</td>
-          <td>10</td>
-          <td>1000</td>
-          <td>3</td>
-        </tr>
-        <tr>
-          <td>제목1</td>
-          <td>작성자1</td>
-          <td>2023.12.12</td>
-          <td>10</td>
-          <td>1000</td>
-          <td>3</td>
-        </tr>
-        <tr>
-          <td>제목1</td>
-          <td>작성자1</td>
-          <td>2023.12.12</td>
-          <td>10</td>
-          <td>1000</td>
-          <td>3</td>
-        </tr>
+        <PostListItem post={defaultPost} />
+        <PostListItem post={defaultPost} />
+        <PostListItem post={defaultPost} />
+        <PostListItem post={defaultPost} />
+        <PostListItem post={defaultPost} />
+        <PostListItem post={defaultPost} />
+        <PostListItem post={defaultPost} />
+        <PostListItem post={defaultPost} />
+        <PostListItem post={defaultPost} />
+        <PostListItem post={defaultPost} />
+        <PostListItem post={defaultPost} />
         </tbody>
       </table>
 
@@ -111,6 +51,31 @@ const PostList = () => {
         </ul>
       </nav>
     </div>
+  )
+}
+
+const PostListItem = ({
+                        post,
+}) => {
+  const {title, user, createdAt, postId} = post;
+
+  const truncateString = (str, maxLength) => {
+    if (str.length > maxLength) {
+      return str.substring(0, maxLength) + '...';
+    } else {
+      return str;
+    }
+  }
+
+  return (
+    <tr onClick={onClick}>
+      <td>{truncateString(title, 20)}</td>
+      <td>{truncateString(user, 15)}</td>
+      <td>{createdAt}</td>
+      <td>3</td>
+      <td>3</td>
+      <td>1000</td>
+    </tr>
   )
 }
 
