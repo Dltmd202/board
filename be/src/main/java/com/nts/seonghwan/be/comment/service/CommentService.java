54c4d921bc9df45f9ea5d92a6d95c00937d5038c
Dltmd202 <dltmd202@gmail.com>
2023-12-13 02:45:10 +0900

    [BE] feat: Create CommentService
    - create a createComment to create a comment

---
+++ be/src/main/java/com/nts/seonghwan/be/comment/service/CommentService.java
@@ -0,0 +1,42 @@
+package com.nts.seonghwan.be.comment.service;
+
+import com.nts.seonghwan.be.comment.dto.CommentCreateRequest;
+import com.nts.seonghwan.be.comment.dto.CommentCreateResponse;
+import com.nts.seonghwan.be.comment.entities.Comment;
+import com.nts.seonghwan.be.comment.exception.InvalidCommenterException;
+import com.nts.seonghwan.be.comment.repository.CommentRepository;
+import com.nts.seonghwan.be.post.entities.Post;
+import com.nts.seonghwan.be.post.exception.NotFoundPostException;
+import com.nts.seonghwan.be.post.repository.PostRepository;
+import com.nts.seonghwan.be.user.entities.User;
+import com.nts.seonghwan.be.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+    private final UserRepository userRepository;
+    private final PostRepository postRepository;
+    private final CommentRepository commentRepository;
+
+    @Transactional()
+    public CommentCreateResponse createComment(CommentCreateRequest commentCreateRequest, Long userId){
+        User commenter = getUserById(userId);
+        Post post = getPostById(commentCreateRequest.getPostId());
+        Comment comment = commentCreateRequest.toEntity(commenter, post);
+        Comment savedComment = commentRepository.save(comment);
+        return CommentCreateResponse.from(savedComment, commenter);
+    }
+
+    private User getUserById(Long userId){
+        return userRepository.findById(userId)
+                .orElseThrow(InvalidCommenterException::new);
+    }
+
+    private Post getPostById(String postId){
+        return postRepository.findByIdWithUser(postId)
+                .orElseThrow(NotFoundPostException::new);
+    }
+}
