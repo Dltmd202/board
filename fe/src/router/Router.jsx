8f59b94833052804995630e70823d69c18390110
Dltmd202 <dltmd202@gmail.com>
2023-12-12 23:04:29 +0900

    [BE] feat: Post detail page routing

--- fe/src/router/Router.jsx
+++ fe/src/router/Router.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import JoinPage from "../pages/JoinPage";
 import LoginPage from "../pages/LoginPage";
 import PostListPage from "../pages/PostListPage";
+import PostDetailPage from "../pages/PostDetailPage";
 
 const Routers = () => {
   return(
@@ -10,6 +11,7 @@ const Routers = () => {
         <Route path="/join" element={<JoinPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/posts" element={<PostListPage />} />
+        <Route path="/posts/:postId" element={<PostDetailPage />} />
       </Routes>
     </BrowserRouter>
   )
