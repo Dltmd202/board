6da6d512a465d2ac4870acccbddc40fafb1f77f7
Dltmd202 <dltmd202@gmail.com>
2023-12-12 19:22:52 +0900

    [FE] fix: Fix postApi
    - Edit post inquiry API call page

--- fe/src/api/post.js
+++ fe/src/api/post.js
@@ -1,5 +1,5 @@
 import {authRequest} from "./index";
 
 export const postApi =  {
-    getPosts: (page) => { return authRequest.get(`/api/v1/posts?size=10&page=${page}`) },
+    getPosts: (page) => { return authRequest.get(`/api/v1/posts?size=10&page=${page - 1}`) },
 }
