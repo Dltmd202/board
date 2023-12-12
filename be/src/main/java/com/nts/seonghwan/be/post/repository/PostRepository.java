9b275751e179946ef639084a875c1735f8e9faa8
Dltmd202 <dltmd202@gmail.com>
2023-12-12 19:54:19 +0900

    [BE] feat: Update PostRepository.java
    - Added query function for viewing single posts

--- be/src/main/java/com/nts/seonghwan/be/post/repository/PostRepository.java
+++ be/src/main/java/com/nts/seonghwan/be/post/repository/PostRepository.java
@@ -4,8 +4,14 @@ import com.nts.seonghwan.be.post.entities.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     Page<Post> findAll(Pageable pageable);
+
+    @Query("select p from Post p join fetch p.user where p.postId = :postId")
+    Optional<Post> findByIdWithUser(String postId);
 }
