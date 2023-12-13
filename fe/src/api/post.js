d2726994cac0f75aae48e63805f12eb6f1ed7751
Dltmd202 <dltmd202@gmail.com>
2023-12-14 01:44:33 +0900

    [FE] feat: Update postApi
    - Function that calls the API that creates a post

--- fe/src/api/post.js
+++ fe/src/api/post.js
@@ -3,4 +3,5 @@ import {authRequest} from "./index";
 export const postApi =  {
     getPosts: (page) => { return authRequest.get(`/api/v1/posts?size=10&page=${page - 1}`) },
     getPost: (postId) => { return authRequest.get(`/api/v1/posts/${postId}`) },
+    createPost: (post) => { return authRequest.post(`/api/v1/posts`, post) },
 }
