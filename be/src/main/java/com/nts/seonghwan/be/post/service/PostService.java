3e52a343d3c052d1348943ec8ff1c8351d43ccb1
Dltmd202 <dltmd202@gmail.com>
2023-12-14 04:39:39 +0900

    [BE] feat: Update PostService
    - Modified to save tags in savePost

--- be/src/main/java/com/nts/seonghwan/be/post/service/PostService.java
+++ be/src/main/java/com/nts/seonghwan/be/post/service/PostService.java
@@ -6,9 +6,12 @@ import com.nts.seonghwan.be.post.dto.PostCreateResponse;
 import com.nts.seonghwan.be.post.dto.PostDetailResponse;
 import com.nts.seonghwan.be.post.dto.PostListResponse;
 import com.nts.seonghwan.be.post.entities.Post;
+import com.nts.seonghwan.be.post.entities.PostTag;
 import com.nts.seonghwan.be.post.exception.InvalidWriterException;
 import com.nts.seonghwan.be.post.exception.NotFoundPostException;
 import com.nts.seonghwan.be.post.repository.PostRepository;
+import com.nts.seonghwan.be.tag.entities.Tag;
+import com.nts.seonghwan.be.tag.repository.TagRepository;
 import com.nts.seonghwan.be.user.entities.User;
 import com.nts.seonghwan.be.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -23,12 +26,20 @@ public class PostService {
     private final PostRepository postRepository;
     private final UserRepository userRepository;
     private final UUIDHolder uuidHolder;
+    private final TagRepository tagRepository;
 
     @Transactional()
     public PostCreateResponse savePost(PostCreateRequest postCreate, Long userId) {
         User writer = getUserById(userId);
         Post post = postCreate.toEntity(writer);
+
         post.grantPostId(uuidHolder);
+        post.tag(
+                postCreate.getTag().stream()
+                    .map(this::getOrDefault)
+                    .map(pt -> new PostTag(post, pt))
+                    .toList());
+
         Post savePost = postRepository.save(post);
         return PostCreateResponse.from(savePost);
     }
@@ -52,6 +63,11 @@ public class PostService {
     private Post getPostById(String postId){
         return postRepository.findByIdWithUser(postId)
                 .orElseThrow(NotFoundPostException::new);
+    }
+
+    private Tag getOrDefault(String tag){
+        return tagRepository.findById(tag)
+                .orElseGet(() -> tagRepository.save(new Tag(tag)));
     }
 
 }
