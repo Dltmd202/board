e7a58350c01d2734b730bf41524b447b247dcc43
Dltmd202 <dltmd202@gmail.com>
2023-12-13 15:52:08 +0900

    [BE] feat: Update Comment.jsx
    - Create comment creation API call function

--- fe/src/api/comment.js
+++ fe/src/api/comment.js
@@ -1,7 +1,10 @@
 import {authRequest} from "./index";
 
 export const commentApi = {
-    getComments: (postId, page) => {
-      return authRequest.get(`/api/v1/posts/${postId}/comments?size=5&page=${page - 1}`)
-    },
+  getComments: (postId, page) => {
+    return authRequest.get(`/api/v1/posts/${postId}/comments?size=5&page=${page - 1}`)
+  },
+  createComment: (postId, content) => {
+    return authRequest.post(`/api/v1/posts/${postId}/comments`, {content: content})
+  }
 }
