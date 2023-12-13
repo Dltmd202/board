38a30791151ed93ac3a9d9407ee7f5a820a62a8e
Dltmd202 <dltmd202@gmail.com>
2023-12-13 23:37:44 +0900

    [BE] feat: Create Exception related by Comment

--- be/src/main/java/com/nts/seonghwan/be/common/error/exception/ErrorCode.java
+++ be/src/main/java/com/nts/seonghwan/be/common/error/exception/ErrorCode.java
@@ -18,6 +18,8 @@ public enum ErrorCode {
 
     // 댓글 관련 에러
     INVALID_COMMENTER(HttpStatus.BAD_REQUEST, "댓글 작성자 정보를 확인해주세요."),
+    NOT_FOUND_COMMENT(HttpStatus.NOT_FOUND, "댓글을 찾을 수 없습니다."),
+    ALREADY_DELETED_COMMENT(HttpStatus.BAD_REQUEST, "이미 삭제된 댓글입니다."),
 
     // 서버 내부 에러
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에 오류가 발생했습니다.");
