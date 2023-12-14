3ef12969875845ef063582a8ee3ccde168bec816
Dltmd202 <dltmd202@gmail.com>
2023-12-15 03:08:24 +0900

    [FE] feat: Create a function to call the API to like or dislike a post

---
+++ fe/src/api/preference.js
@@ -0,0 +1,6 @@
+import {authRequest} from "./index";
+
+export const preferenceApi = {
+  like: (postId) => { return authRequest.post(`/api/v1/posts/${postId}/preference?type=LIKE`) },
+  unlike: (postId) => { return authRequest.delete(`/api/v1/posts/${postId}/preference?type=UNLIKE`) }
+}
