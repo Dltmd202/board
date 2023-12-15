aa835bd6370035136b9288b92506e63a98253ce9
Dltmd202 <dltmd202@gmail.com>
2023-12-16 00:19:52 +0900

    [FE] fix: Edit PrivateRouter.jsx

--- fe/src/router/PrivateRouter.jsx
+++ fe/src/router/PrivateRouter.jsx
@@ -3,7 +3,7 @@ import {Navigate, Outlet} from "react-router-dom";
 const PrivateRouter = () => {
   const token = localStorage.getItem('token');
 
-  return !token ? <Navigate to="/login" /> : <Outlet />;
+  return token ? <Navigate to="/login" /> : <Outlet />;
 }
 
 export default PrivateRouter;
