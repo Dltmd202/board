612622aca4a7f66a0e4e5b3e7e26803cdc68b6a5
Dltmd202 <dltmd202@gmail.com>
2023-12-16 00:31:34 +0900

    [FE] feat: Update createAuthRequest
    - Clearing token in case of 401 response in response interceptor

--- fe/src/api/index.js
+++ fe/src/api/index.js
@@ -8,17 +8,29 @@ const createRequest = () => {
   return instance;
 }
 
-const setAuthInterceptors = () => {
+const createAuthRequest = () => {
   const instance = createRequest();
-  const sessionId = localStorage.getItem("TOKEN");
 
   instance.interceptors.request.use(
     (config) => {
+      const sessionId = localStorage.getItem("TOKEN");
       config.headers["Authorization"] = `Bearer ${sessionId}`;
       return config;
     },
     (error) => Promise.reject(error.response),
   );
+
+  instance.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    async (error) => {
+      if(error.response.status === 401) {
+        await localStorage.removeItem("TOKEN");
+        await localStorage.removeItem("NAME");
+      }
+      return Promise.reject(error.response);
+    });
 
   return instance;
 };
@@ -26,4 +38,4 @@ const setAuthInterceptors = () => {
 
 export const request = createRequest();
 
-export const authRequest = setAuthInterceptors();
+export const authRequest = createAuthRequest();
