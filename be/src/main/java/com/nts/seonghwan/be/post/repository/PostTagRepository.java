ebe86c1d0a7138d7d5ad978473394631e56ee8cb
Dltmd202 <dltmd202@gmail.com>
2023-12-15 20:14:47 +0900

    [BE] feat: Create PostTagRepository

---
+++ be/src/main/java/com/nts/seonghwan/be/post/repository/PostTagRepository.java
@@ -0,0 +1,7 @@
+package com.nts.seonghwan.be.post.repository;
+
+import com.nts.seonghwan.be.post.entities.PostTag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostTagRepository extends JpaRepository<PostTag, Long>{
+}
