e24bdd93d5b28b2af7ac5e889698ed6248b98f7a
Dltmd202 <dltmd202@gmail.com>
2023-12-15 18:55:44 +0900

    [FE] feat: Update postApi
    - Post processing API call function to delete a post

--- fe/src/api/post.js
+++ fe/src/api/post.js
@@ -4,4 +4,5 @@ export const postApi =  {
     getPosts: (page) => { return authRequest.get(`/api/v1/posts?size=10&page=${page - 1}`) },
     getPost: (postId) => { return authRequest.get(`/api/v1/posts/${postId}`) },
     createPost: (post) => { return authRequest.post(`/api/v1/posts`, post) },
+    deletePost: (postId) => { return authRequest.delete(`/api/v1/posts/${postId}`) },
 }
