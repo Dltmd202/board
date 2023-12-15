2380a332c91bbc7e48d0bb5f59156a010469ae1a
Dltmd202 <dltmd202@gmail.com>
2023-12-16 00:12:58 +0900

    [FE] feat: Creat PrivateRouter

---
+++ fe/src/router/PrivateRouter.jsx
@@ -0,0 +1,9 @@
+import {Navigate, Outlet} from "react-router-dom";
+
+const PrivateRouter = () => {
+  const token = localStorage.getItem('token');
+
+  return !token ? <Navigate to="/login" /> : <Outlet />;
+}
+
+export default PrivateRouter;
